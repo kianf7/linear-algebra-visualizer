f8926183211afd59b3d264ab3605916a398bcb68
kianf7 <kian.fitzkr@gmail.com>
2025-06-24 00:44:55 +0200

    Matrix Klasse weitér gemacht #gurt

--- LinearAlgebraVisualizer/src/Matrix2D.java
+++ LinearAlgebraVisualizer/src/Matrix2D.java
@@ -1,2 +1,30 @@
 public class Matrix2D {
+
+    private double a,b,c,d;
+
+    public Matrix2D(double a, double b, double c, double d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    public double getA() {
+        return a;
+    }
+    public double getB() {
+        return b;
+    }
+    public double getC() {
+        return c;
+    }
+    public double getD() {
+        return d;
+    }
+
+    public Vector2D transformVector(Vector2D v) {
+        double transformedX = a * v.getX() + b * v.getY();
+        double transformedY = c * v.getX() + d * v.getY();
+        return new Vector2D(transformedX, transformedY);
+    }
 }
