13cf80a5ac7bd2c2e43037850f0ea2b1e2b8951f
Kian <kian.fitzkr@gmail.com>
2025-06-24 16:25:10 +0200

    jetzt

--- LinearAlgebraVisualizer/src/GUI.java
+++ LinearAlgebraVisualizer/src/GUI.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
 }
