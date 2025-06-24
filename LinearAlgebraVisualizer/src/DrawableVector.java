7f1953d8f5152283f5067fc26a98f5dceb22bad9
Lukas <lukas.ritter06@gmail.com>
2025-06-24 16:31:28 +0200

    Draw Vector Class

---
+++ LinearAlgebraVisualizer/src/DrawableVector.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class DrawableVector extends Vector2D {
+    private Color color;
+
+    public DrawableVector(double x, double y) {
+        super(x, y);
+    }
+
+    public void draw(Graphics g) {
+
+
+    }
+}
--- LinearAlgebraVisualizer/src/Matrix2D.java
+++ LinearAlgebraVisualizer/src/Matrix2D.java
@@ -1,7 +1,8 @@
 public class Matrix2D {
 
     private double a,b,c,d;
-
+    // M = a, b
+    //     c, d
     public Matrix2D(double a, double b, double c, double d) {
         this.a = a;
         this.b = b;
