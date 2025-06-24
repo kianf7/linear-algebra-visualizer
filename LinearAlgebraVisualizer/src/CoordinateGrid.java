ef1c8eed03d729fc0c392524d1a6024d8ae68fa7
kianf7 <kian.fitzkr@gmail.com>
2025-06-24 21:45:00 +0200

    CoordinateGrid draw method. Origin not drawn properly

--- LinearAlgebraVisualizer/src/CoordinateGrid.java
+++ LinearAlgebraVisualizer/src/CoordinateGrid.java
@@ -1,13 +1,41 @@
 import java.awt.*;
 
 public class CoordinateGrid implements Drawable{
-    private int scale;
+    private final int gridSpacing;
+    private final int windowWidth;
+    private final int windowHeight;
 
-    public CoordinateGrid(int scale) {
-        this.scale = scale;
+
+    public CoordinateGrid(int scale, int windowWidth, int windowHeight) {
+        gridSpacing = scale;
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
     }
+
     @Override
     public void draw(Graphics2D g) {
-        //TODO
+        drawGrid(g);
+        drawOrigin(g);
+    }
+
+    public void drawGrid(Graphics2D g) {
+        g.setColor(Color.gray);
+
+        for (int x = 0; x <= windowWidth; x+= gridSpacing) {
+            g.drawLine(x, 0, x, windowHeight);
+        }
+
+        for (int y = 0; y <= windowHeight; y+= gridSpacing) {
+            g.drawLine(0, y, windowWidth, y);
+        }
+    }
+
+    public void drawOrigin(Graphics2D g) {
+        g.setColor(Color.white);
+        int originX = windowWidth / 2;
+        int originY = windowHeight / 2;
+
+        g.drawLine(originX, 0, originX, windowHeight);
+        g.drawLine(0, originY, windowWidth, originY);
     }
 }
--- LinearAlgebraVisualizer/src/GUI.java
+++ LinearAlgebraVisualizer/src/GUI.java
@@ -5,6 +5,7 @@ public class GUI extends JPanel {
     private int scale;
 
     GUI(int scale) {
+        setBackground(Color.BLACK);
         this.scale = scale;
     }
 
@@ -13,10 +14,17 @@ public class GUI extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         //TODO add everything that should be drawn
+        CoordinateGrid gridTest = new CoordinateGrid(scale, getWidth(), getHeight());
+        gridTest.draw(g2d);
+
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Linear Algebra Visualizer");
+        GUI testGui = new GUI(35);
+        frame.add(testGui);
+
+
         frame.setSize(800,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
