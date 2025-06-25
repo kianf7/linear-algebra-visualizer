f8d8e9d8d5965d3eb41f2c81989c13b5d3a90e37
Lukas <lukas.ritter06@gmail.com>
2025-06-25 16:27:16 +0200

    Vector2D: vectorAdd methode DrawableVector: drawVectorSum

--- LinearAlgebraVisualizer/src/GUI.java
+++ LinearAlgebraVisualizer/src/GUI.java
@@ -23,14 +23,20 @@ public class GUI extends JPanel {
         //TODO add everything that should be drawn
         CoordinateGrid gridTest = new CoordinateGrid();
         gridTest.draw(g2d, getWidth(), getHeight(), scale);
-        DrawableVector test = new DrawableVector(1.5,0, Color.yellow);
-        test.draw(g2d, getWidth(), getHeight(), scale);
+
+        //DrawableVector test = new DrawableVector(1.5,0, Color.yellow);
+        //test.draw(g2d, getWidth(), getHeight(), scale);
+
+        //DrawableVector test2 = new DrawableVector(2,4, Color.green);
+        //test2.draw(g2d, getWidth(), getHeight(), scale);
+
+        DrawableVector.drawVectorSum(g2d, new DrawableVector(1,2, Color.red), new DrawableVector(1, -2,Color.green),getWidth(),getHeight(), scale);
 
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Linear Algebra Visualizer");
-        GUI testGui = new GUI(35);
+        GUI testGui = new GUI(60);
         frame.add(testGui);
         frame.setSize(800,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
