1d009135363494221a11d453e7c47170350993e7
Lukas <lukas.ritter06@gmail.com>
2025-06-25 16:25:21 +0200

    Vector2D: vectorAdd methode DrawableVector: drawVectorSum

--- LinearAlgebraVisualizer/src/DrawableVector.java
+++ LinearAlgebraVisualizer/src/DrawableVector.java
@@ -13,9 +13,35 @@ public class DrawableVector extends Vector2D implements Drawable {
         int originX = windowWidth / 2;
         int originY = windowHeight / 2;
         g.setColor(color);
-        int endX= (int) (originX + scale * (int) getX());
+        int endX= (int) (originX + scale * getX());
         int endY= (int) (originY - scale * getY());
+        g.setStroke(new BasicStroke(3));
         g.drawLine(originX, originY, endX, endY);
+    }
+    public static void drawVectorSum(Graphics2D g, DrawableVector v1, DrawableVector v2, int windowWidth, int windowHeight, int scale) {
+
+        // draw v1 starting from origin
+        int originX_v1 = windowWidth / 2;
+        int originY_v1 = windowHeight / 2;
+
+        int endX_v1 = (int) (originX_v1 + scale * v1.getX());
+        int endY_v1 = (int) (originY_v1 - scale * v1.getY());
+
+        g.setColor(v1.color);
+        g.setStroke(new BasicStroke(3));
+        g.drawLine(originX_v1, originY_v1, endX_v1, endY_v1);
+
+        // set v2 origin based on ending point of v1
+
+        int endX_v2 = (int) (endX_v1 + scale * v2.getX());
+        int endY_v2 = (int) (endY_v1 - scale * v2.getY());
+
+        g.setColor(v2.color);
+        g.setStroke(new BasicStroke(3));
+        g.drawLine(endX_v1, endY_v1, endX_v2, endY_v2);
+
 
     }
+
+
 }
--- LinearAlgebraVisualizer/src/Vector2D.java
+++ LinearAlgebraVisualizer/src/Vector2D.java
@@ -1,5 +1,3 @@
-import java.awt.*;
-
 public class Vector2D{
     private double x;
     private double y;
@@ -18,6 +16,19 @@ public class Vector2D{
     public double getX() {return x;}
     public double getY() {return y;}
 
+    public static Vector2D vectorAdd(Vector2D v1, Vector2D v2) {
+        return new Vector2D(v1.getX()+ v2.getX(), v1.getY() + v2.getY());
+    }
 
+    /* TEST
+    public static void main(String[] args) {
+        Vector2D v1 = new Vector2D(1.0, 2.0);
+        Vector2D v2 = new Vector2D(3.0, 4.0);
+
+        Vector2D v3 = Vector2D.vectorAdd(v1, v2);
+        System.out.println("x: " + v3.getX() + " y: " + v3.getY());
+
+    }
+    */
 
 }
