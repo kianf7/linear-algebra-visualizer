af4268358f767e0c470f446d675d0f12aaee635b
Lukas <lukas.ritter06@gmail.com>
2025-06-27 17:07:37 +0200

    merge VectorManager

--- LinearAlgebraVisualizer/src/VectorManager.java
+++ LinearAlgebraVisualizer/src/VectorManager.java
@@ -34,7 +34,40 @@ public class VectorManager implements Drawable{
     @Override
     public void draw(Graphics2D g, int windowWidth, int windowHeight, int scale) {
         for(DrawableVector vector : inputVectors) {
+            vector.setOriginX(windowWidth / 2);
+            vector.setOriginY(windowHeight / 2);
             vector.draw(g, windowWidth, windowHeight, scale);
         }
+    }
+    public void drawSum(Graphics2D g, int windowWidth, int windowHeight, int scale) {
+        double sumX = 0;
+        double sumY = 0;
+        int originX = windowWidth / 2;
+        int originY = windowHeight / 2;
+
+        if (inputVectors.isEmpty()) {
+            return;
+        }
+        for (DrawableVector v : inputVectors) {
+            if (inputVectors.size() == 1) {
+                v.setOriginX(originX);
+                v.setOriginY(originY);
+                v.draw(g, windowWidth, windowHeight, scale);
+                return;
+            }
+            v.setOriginX(originX);
+            v.setOriginY(originY);
+            v.draw(g, windowWidth, windowHeight, scale);
+
+            originX = v.calculateEndX(v, scale);
+            originY = v.calculateEndY(v, scale);
+
+            sumX += v.getX();
+            sumY += v.getY();
+        }
+        DrawableVector sum = new DrawableVector(sumX,sumY,Color.blue,"sum");
+        sum.setOriginX(windowWidth / 2);
+        sum.setOriginY(windowHeight / 2);
+        sum.draw(g,windowWidth,windowHeight,scale);
     }
 }
